eb665b6b74c7e820634015950cc3cfa3fd078fad
Oriol Puig <oriol.puig90@gmail.com>
2017-03-03 14:22:11 +0100

    Prepare starter kit: - Add webpack, webpack-dev-server and some loaders - Add babel and some presets - Add react, react-dom and react-router - Write a first component

---
+++ src/HelloComponent.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export const HelloComponent = () => {
+    return (
+        <div>
+            <div>
+                <h2>Welcome to React</h2>
+            </div>
+            <p >
+                To get started, edit <code>src/HelloComponent.js</code> and save to reload.
+            </p>
+        </div>
+    );
+}
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    context: path.join(__dirname, 'src'),
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    entry: {
+        app: './index.js',
+        vendor: [
+            'react',
+            'react-dom',
+            'react-router',
+        ]
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[chunkhash].[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+        ]
+    },
+    // For development https://webpack.js.org/configuration/devtool/#for-development
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 8080,
+        noInfo: true,
+    },
+    plugins: [
+        // Caching vendors with manifest
+        // https://webpack.js.org/guides/code-splitting-libraries/#manifest-file
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor', 'manifest'],
+        }),
+        //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
+        new HtmlWebpackPlugin({
+            filename: 'index.html', //Name of file in ./dist/
+            template: 'index.html', //Name of template in ./src
+            hash: true
+        })
+    ],
+};
