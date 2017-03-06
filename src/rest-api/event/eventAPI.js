befdf8346f8dfaa0177b2f376bf3ebe87c7976b2
Oriol Puig <oriol.puig90@gmail.com>
2017-03-06 15:50:57 +0100

    Create event API calls - Generate events mocked data - Create a eventAPI with get all and getById methods
    Close #4

---
+++ src/rest-api/event/eventAPI.js
@@ -0,0 +1,22 @@
+import { eventsMockData } from './eventMockData';
+
+// Fake API using es6 Promises polyfill (with babel-preset-env).
+// In future, we can replace by real one.
+class EventAPI {
+    events = [];
+
+    constructor() {
+        this.events = eventsMockData;
+    }
+
+    fetchEvents() {
+        return Promise.resolve(this.events);
+    }
+
+    fetchEventById(id) {
+        const event = this.events.find(e => e.id === id);
+        return Promise.resolve(event);
+    }
+}
+
+export const eventAPI = new EventAPI();
